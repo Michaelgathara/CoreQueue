727c9cce8fd5876a2a3be7c9d6691b67a12947d7
michaelgathara <mikegtrm@gmail.com>
2025-10-26 18:03:13 -0500

    feat: added models

--- apps/api/models/__init__.py
+++ apps/api/models/__init__.py
@@ -1,5 +1,9 @@
 from apps.api.models.base import Base
 from apps.api.models.job import Job
+from apps.api.models.team import Team
+from apps.api.models.user import User
+from apps.api.models.runner import Runner
+from apps.api.models.metric import RunnerMetric
 
-__all__ = ["Base", "Job"]
+__all__ = ["Base", "Job", "Team", "User", "Runner", "RunnerMetric"]
 
